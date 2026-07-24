4422ef9530761a3d1cb343c0208b0355376b1b47
BelGray <belg186@yandex.ru>
2026-07-24 22:41:43 +0300

    fix bugs

--- telegram_suggestions/main.py
+++ telegram_suggestions/main.py
@@ -2,37 +2,37 @@ import asyncio
 import logging
 
 from aiogram import Bot, Dispatcher
+from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 
-from config import config
-from database.engine import init_db
-from handlers import channel_setup, subscriber, admin_settings, admin_replies, payments
+from telegram_suggestions.config import config
+from telegram_suggestions.database.engine import init_db
+from telegram_suggestions.handlers import channel_setup, subscriber, admin_settings, admin_replies, payments
 
 
 async def main():
-    # Настройка логов
     logging.basicConfig(
         level=logging.INFO,
         format="%(asctime)s - %(levelname)s - %(name)s - %(message)s"
     )
 
-    # Инициализация SQLite базы данных
     await init_db()
 
-    # Инициализация Бота и Диспетчера
-    bot = Bot(token=config.BOT_TOKEN)
+    # Включаем HTML разметку глобально по умолчанию для всего бота
+    bot = Bot(
+        token=config.BOT_TOKEN,
+        default=DefaultBotProperties(parse_mode=ParseMode.HTML)
+    )
     dp = Dispatcher()
 
-    # Регистрация роутеров хэндлеров
     dp.include_router(channel_setup.router)
     dp.include_router(subscriber.router)
     dp.include_router(admin_settings.router)
     dp.include_router(admin_replies.router)
     dp.include_router(payments.router)
 
-    logging.info("🚀 Бот Onward успешно запущен и готов к работе!")
+    logging.info("🚀 Бот Onward успешно запущен и готов к работе (HTML ParseMode)!")
 
-    # Удаляем вебхуки и запускаем поллинг
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
 
